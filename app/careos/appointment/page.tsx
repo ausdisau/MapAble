615555ed9f175eb943e6dcf7c92eee0c11f42c45
ausdisau <australiandisabilitycontact@gmail.com>
2026-07-13 20:36:48 +1000

    feat: add CareOS appointment mission page

---
+++ app/careos/appointment/page.tsx
@@ -0,0 +1,9 @@
+import { CareOSAppointmentMission } from "@/components/intelligence/CareOSAppointmentMission";
+
+export default function CareOSAppointmentPage() {
+  return (
+    <main className="mx-auto w-full max-w-6xl px-4 py-8 sm:px-6 lg:px-8">
+      <CareOSAppointmentMission />
+    </main>
+  );
+}
